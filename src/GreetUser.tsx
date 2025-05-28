67c4b946e7fbeb8c2e0682c09b0a8bf8eeae2a6c
marcolilli <marco@v1b.es>
2025-05-28 15:44:36 +0200

    Add build workflow

--- src/GreetUser.tsx
+++ src/GreetUser.tsx
@@ -2,7 +2,7 @@
 export default function GreetUser() {
     return (
         <div>
-            <h1>Hello</h1>
+            <h1>Hello!!</h1>
             <p>Welcome to our application!</p>
         </div>
     );
